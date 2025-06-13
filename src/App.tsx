f4e78a4f1088d2036ed48161bf6e91514c037b84
adnanaq <adnan_aq@hotmail.com>
2025-06-13 16:34:57 -0400

    Remove animated background

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { AppRoutes } from './routes/AppRoutes'
-import { AnimatedBackground } from './components/AnimatedBackground'
-import { ThemeTransition } from './components/ThemeTransition'
+import { AppRoutes } from "./routes/AppRoutes";
+import { ThemeTransition } from "./components/ThemeTransition";
 
 function App() {
   return (
     <div className="App relative min-h-screen transition-theme">
-      <AnimatedBackground />
       <AppRoutes />
       <ThemeTransition />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
